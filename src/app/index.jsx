50d7c62e5e9294c7b46233ed15d2d8d16df2383d
alnord.wang <alnord.wang@grapecity.com>
2018-07-16 12:03:13 +0800

    Add source-map support

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import {render} from 'react-dom';
+class PlaceHolder extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+  onClick(evt) {
+    console.log(evt);
+  }
 
+  render() {
+    return(
+      <div id="placeholder" onClick={this.onClick}>Click Me!</div>
+    )
+  }
+}
 const App = () => {
   return (
-      <p>Hello World</p>
+    <div id='container'>
+      <PlaceHolder />
+    </div>
   );
 }
 
